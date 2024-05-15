260d16f537e6258f3ddaac0cad5c3ac7a124276b
John Gaitho <johngaitho05@gmail.com>
2024-05-15 12:48:45 +0300

    replace required with import

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
-// server.js
-
-const express = require('express');
-const router = require('./routes/index');
+import express from 'express';
+import router from './routes/index';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Load all routes from the file routes/index.js
 app.use(router);
 
 app.listen(PORT, () => {
